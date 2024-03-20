bbb76ac409987688abf44e0230d623428d647049
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-03-20 16:45:57 +0100

    Init - Hello Vue Vite

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+
+export default {
+
+  //  data
+  data() {
+    return {
+      text: 'Hello VUE/VITE'
+    }
+  }
+  // methods
+  // mounted
+  // other lifecycle hooks
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+<style>
+body{
+  background-color: azure;
+}
+
+h1{
+  text-align: center;
+  color: darkblue;
+}
+</style>
